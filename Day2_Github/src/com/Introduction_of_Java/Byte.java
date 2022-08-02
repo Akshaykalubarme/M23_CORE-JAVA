8a6a23d4ed2929ff20351ab686e20b5da9141016
Lenovo <Lenovo@LAPTOP-4FF204OR>
2022-08-02 15:47:49 +0530

    first commit

---
+++ Day2_Github/src/com/Introduction_of_Java/Byte.java
@@ -0,0 +1,12 @@
+package com.Introduction_of_Java;
+
+public class Byte {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a=127;
+		System.out.println(a);
+
+	}
+
+}
