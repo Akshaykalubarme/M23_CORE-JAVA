1bc9180f8b3361c73c253b16e917cfad6bb22396
Lenovo <Lenovo@LAPTOP-4FF204OR>
2022-08-08 08:03:07 +0530

    First Commit

--- Assignment -1  Operators, Decision Making and Control Structures/src/com/cg/operators/LogicalOperator.java
+++ Assignment -1  Operators, Decision Making and Control Structures/src/com/cg/operators/LogicalOperator.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class LogicalOperator {
 
-	@SuppressWarnings("resource")
+	
 	public static void main(String[] args) {
 		
 		  Scanner sc=new Scanner(System.in);
 	       int a=sc.nextInt();
 	       int b=sc.nextInt();
 	       int c=sc.nextInt();
-	  
+	       sc.close();
 		    // Logical AND
 	        if ((a < b) && (b > c)) {
 	          
---
+++ Assignment-2_Function And Recursion/src/com/FibonacciSeriesUsingRecursion/SumOfDigitsUsingRecursion.java
@@ -0,0 +1,29 @@
+package com.FibonacciSeriesUsingRecursion;
+
+import java.util.Scanner;
+
+public class SumOfDigitsUsingRecursion {
+	
+    // of digit using recursion
+    static int sum_of_digit(int num)
+    {
+        if (num == 0)
+            return 0;
+        return (num % 10 + sum_of_digit(num / 10));
+    }
+ 
+   
+    public static void main(String args[])
+    {
+    	 Scanner cs=new Scanner(System.in);
+         int num = 0;
+		 System.out.print("Enter the N value:");
+		 num=cs.nextInt();
+        int result = sum_of_digit(num);
+        System.out.println("Sum of digits is: " + result);
+    }
+	
+
+	}
+
+
